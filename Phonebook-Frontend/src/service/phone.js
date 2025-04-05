c7e4139d9e120d4290499b5e277cf4664bb67716
unknown <kumutimam@gmail.com>
2025-04-05 22:49:33 +0200

    Your update message here

--- Phonebook-Frontend/src/service/phone.js
+++ Phonebook-Frontend/src/service/phone.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const baseUrl  = 'http://localhost:3001/person/';
+const baseUrl  = 'http://localhost:3001/';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
