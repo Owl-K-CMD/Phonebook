db7bce742ec7913867febe551063199f60cc7de1
unknown <kumutimam@gmail.com>
2025-04-05 23:54:45 +0200

    Moon landing was fake

--- Phonebook-Frontend/src/service/phone.js
+++ Phonebook-Frontend/src/service/phone.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const baseUrl  = 'http://localhost:3001/api/info';
+const baseUrl  = 'http://localhost:3001/api/person';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
