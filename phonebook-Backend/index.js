c10b38f4d824c806d0484d43320a9e47cd27a5eb
unknown <kumutimam@gmail.com>
2025-04-07 18:59:11 +0200

    Added frontend and backend

--- phonebook-Backend/index.js
+++ phonebook-Backend/index.js
@@ -11,27 +11,27 @@ const persons = [
   {
     id: "1",
     name: "Arto Hellas",
-    number: "040-123456"
+    phoneNumber: "040-123456"
   },
   {
     id: "2",
     name: "Ada Lovelace",
-    number: "39-44-5323523"
+    phoneNumber: "39-44-5323523"
   },
   {
     id: "3",
     name: "Dan Abramov",
-    number: "12-43-234345"
+    phoneNumber: "12-43-234345"
   },
   {
     id: "4",
     name: "Mary Poppendieck",
-    number: "7675782910"
+    phoneNumber: "7675782910"
   },
   {
     id: "5",
     name: "Penelope",
-    number: "12345678909"
+    phoneNumber: "12345678909"
   }
 ]
 
