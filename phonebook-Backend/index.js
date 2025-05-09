cf86f9112069fc52177d8ea9455d593707c0a980
unknown <kumutimam@gmail.com>
2025-05-09 13:20:24 +0200

    Implemented backend logic for notes

--- phonebook-Backend/index.js
+++ phonebook-Backend/index.js
@@ -16,6 +16,7 @@ const requestLogger = (request, response, next) => {
 app.use(express.json())
 app.use(requestLogger)
 app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'dist')))
 
 
 
